54a3a98cdea6b52589870870e62c4030d6a490c6
Joe3160 <694995669@qq.com>
2018-01-23 10:21:06 +0800

    dd

---
+++ src/day01/Demo.java
@@ -0,0 +1,10 @@
+package day01;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Integer的最大值：" + Integer.MAX_VALUE);
+        System.out.println("Integer的最小值：" + Integer.MIN_VALUE);
+        int i=Integer.parseInt("123");
+        System.out.println(i);
+    }
+}
---
+++ src/day01/readme.md
@@ -0,0 +1,25 @@
+#### 第1节 基本数据类型的封装类
+
+1. 查看api
+
+2. 8大数据类型
+
+
+
+|  类型  |  类    |
+|-------|---------|
+|byte   |Byte     |
+|char   |Character|
+|short  |Short    |
+|int    |Integer  |
+|long   |Long     |
+|float  |Float    |
+|double |Double   |
+|boolean|Boolean  |
+
+
+
+
+
+
+
