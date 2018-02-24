d8bed34512921c21037b488d234822ff0a0ea46b
Joe3160 <694995669@qq.com>
2018-02-24 18:03:58 +0800

    ArrayList

---
+++ src/day29/Demo.java
@@ -0,0 +1,17 @@
+package day29;
+
+import java.util.ArrayList;
+
+public class Demo {
+    public static void main(String[] args){
+        ArrayList<String> all=new ArrayList<String>();
+        all.add("a");
+        all.add("b");
+        all.add("c");
+        all.add("d");
+        all.add("e");
+        System.out.println("all 的长度是:"+all.size());
+        System.out.println("all 的内容："+all);
+
+    }
+}
---
+++ src/day29/readme.md
@@ -0,0 +1,7 @@
+#### 第29节 集合实现类 ArrayList
+
+##### 集合实现类 ArrayList
+
+1. ArrayList
+    
+    数组列表
